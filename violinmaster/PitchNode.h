57d6c1a730725f9077c8bd6d65d40c499b8660f2
Jian Wu <jian.wu@nxp.com>
2015-09-22 02:02:42 +0800

    add pitch desc

--- violinmaster/PitchNode.h
+++ violinmaster/PitchNode.h
@@ -6,3 +6,25 @@ private:
   char       syllableName[4];
   PitchNode  next;
 };
+
+class PitchWindow
+{
+public:
+  PitchWindow():pos(0) { memset (ring, 0, WINDOW_SIZE); }
+  void insert ()
+
+private:
+  const unsigned int WINDOW_SIZE; 
+  char ring[WINDOW_SIZE];
+  unsigned int pos;
+};
+
+
+class PitchDesc
+{
+
+private:
+  const char pitchName[][]   = { "Ab", "A", "A#", "Bb", "B", "B#", "Cb", "C", "C#", "Db", "D", "D#", "Eb", "E", "E#", "Fb", "F", "F#", "Gb", "G", "G#" };
+  const char sylableName[][] = { "la", "la","la", "si", "si","si", "si", "do","do", "re", "re","re", "mi", "mi","mi", "fa","fa", "fa", "sol","sol","sol" };
+  
+};
