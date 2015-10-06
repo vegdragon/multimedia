dee00dff7c476a2702473ac943676d3cf3b26b18
jian <vegdragon@gmail.com>
2015-10-06 13:48:29 +0800

    refactoring - create Pitch.cpp

--- master/Pitch.h
+++ master/Pitch.h
@@ -10,7 +10,7 @@
 class Pitch
 {
 public:
-	Pitch() : _next(NULL) {}
+	Pitch();
 
 	Pitch(pitch_idx_t  idx,
 				pitch_freq_t freq,
@@ -18,54 +18,17 @@ public:
 				pitch_freq_t maxPosDev,
 				pitch_freq_t maxNegDev,
 				pitch_freq_t avgPosDev,
-				pitch_freq_t avgNegDev) : _next(NULL)
-	{
-		_pitchIndex = idx;
-		_frequency = freq;
-		_totalPitchCounter = counter;
-		_maxNegativeDeviation = maxPosDev;
-		_maxPositiveDeviation = maxNegDev;
-		_avgNegativeDeviation = avgPosDev;
-		_avgPositiveDeviation = avgNegDev;
-		_duration = DURATION_PER_SAMPLE_IN_MS * _totalPitchCounter;
-	}
+				pitch_freq_t avgNegDev);
 
-	Pitch(Pitch & pitchNode) : _next(NULL)
-	{
-		_pitchIndex = pitchNode._pitchIndex;
-		_frequency = pitchNode._frequency;
-		_totalPitchCounter = pitchNode._totalPitchCounter;
-		_maxNegativeDeviation = pitchNode._maxNegativeDeviation;
-		_maxPositiveDeviation = pitchNode._maxPositiveDeviation;
-		_avgNegativeDeviation = pitchNode._avgNegativeDeviation;
-		_avgPositiveDeviation = pitchNode._avgPositiveDeviation;
-		_duration = DURATION_PER_SAMPLE_IN_MS * pitchNode._totalPitchCounter;
-	}
+	Pitch(Pitch & pitchNode);
 
-	double maxPosDeviationPercentage()
-	{
-		PitchDictionary pd;
-		return _maxPositiveDeviation * 100.00 / pd.indexToFrequency(_pitchIndex);
-	}
-	double maxNegDeviationPercentage()
-	{
-		PitchDictionary pd;
-		return _maxNegativeDeviation * 100.00 / pd.indexToFrequency(_pitchIndex);
-	}
-
-	double avgPosDeviationPercentage()
-	{
-		PitchDictionary pd;
-		return _avgPositiveDeviation * 100.00 / pd.indexToFrequency(_pitchIndex);
-	}
-	double avgNegDeviationPercentage()
-	{
-		PitchDictionary pd;
-		return _avgNegativeDeviation * 100.00 / pd.indexToFrequency(_pitchIndex);
-	}
+	double maxPosDeviationPercentage();
+	double maxNegDeviationPercentage();
+	double avgPosDeviationPercentage();
+	double avgNegDeviationPercentage();
 
 public:
-	static const pitch_duration_t DURATION_PER_SAMPLE_IN_MS = 1024.00 * 1000.00 / 44100.00;
+	static const pitch_duration_t DURATION_PER_SAMPLE_IN_MS;
     pitch_idx_t  	_pitchIndex;
     pitch_freq_t 	_frequency;
     int 				_totalPitchCounter;
@@ -74,39 +37,16 @@ public:
     pitch_freq_t 	_avgPositiveDeviation;
     pitch_freq_t 	_avgNegativeDeviation;
     pitch_duration_t _duration;
-    Pitch * _next;
+    Pitch * 			_next;
 };
 
 class PitchNodeList
 {
 public:
-	PitchNodeList() {}
-	void push(Pitch * pNode)
-	{
-		if (_tail != NULL)
-		{
-			_tail->_next = pNode;
-			pNode->_next = NULL;
-			_tail = pNode;
-		}
-		else
-		{
-			pNode->_next = NULL;
-			_head = pNode;
-			_tail = pNode;
-		}
-	}
-
-	Pitch * head()
-	{
-		return _head;
-	}
-
-	Pitch * last()
-	{
-		return _tail;
-	}
-
+	PitchNodeList();
+	void push(Pitch * pNode);
+	Pitch * head();
+	Pitch * last();
 private:
 	Pitch * _head;
 	Pitch * _tail;
@@ -115,60 +55,9 @@ private:
 class Music
 {
 public:
-	void insertAndMergeANote (Pitch * pPitch)
-	{
-		Pitch * pLastPitch = _pitchNodeList.last();
-		if (NULL != pLastPitch)
-		{
-			if (pLastPitch->_pitchIndex == pPitch->_pitchIndex)
-			{
-				// same pitch as the last one, merge the _duration
-				pLastPitch->_maxNegativeDeviation = MIN(pLastPitch->_maxNegativeDeviation, pPitch->_maxNegativeDeviation);
-				pLastPitch->_maxPositiveDeviation = MAX(pLastPitch->_maxPositiveDeviation, pPitch->_maxPositiveDeviation);
-				pLastPitch->_avgNegativeDeviation = (pLastPitch->_avgNegativeDeviation * pLastPitch->_totalPitchCounter + pPitch->_avgNegativeDeviation * pPitch->_totalPitchCounter)
-						/ (pLastPitch->_totalPitchCounter + pPitch->_totalPitchCounter);
-				pLastPitch->_avgPositiveDeviation = (pLastPitch->_avgPositiveDeviation * pLastPitch->_totalPitchCounter + pPitch->_avgPositiveDeviation * pPitch->_totalPitchCounter)
-										/ (pLastPitch->_totalPitchCounter + pPitch->_totalPitchCounter);
-				pLastPitch->_totalPitchCounter += pPitch->_totalPitchCounter;
-				pLastPitch->_duration = Pitch::DURATION_PER_SAMPLE_IN_MS * pLastPitch->_totalPitchCounter;
-
-				delete pPitch;
-				pPitch = NULL;
-			}
-			else
-			{
-				_pitchNodeList.push(pPitch);
-			}
-		}
-		else
-		{
-			_pitchNodeList.push(pPitch);
-		}
-	}
-
-	PitchNodeList & getNodeList()
-	{
-		return _pitchNodeList;
-	}
-
-	void display ()
-	{
-		Pitch * pPitch = _pitchNodeList.head();
-		PitchDictionary pd;
-		while (pPitch != NULL)
-		{
-			printf ("Detected Note(%s), _duration(%lf).\n",
-					pd.indexToPitchName(pPitch->_pitchIndex),
-					pPitch->_duration);
-			printf ("PitchNode Statistics: %.2lf[%.2lf%%], %.2lf[%.2lf%%], %.2lf[%.2lf%%], %.2lf[%.2lf%%]\n",
-					pPitch->_avgNegativeDeviation, pPitch->avgNegDeviationPercentage(),
-					pPitch->_avgPositiveDeviation, pPitch->avgPosDeviationPercentage(),
-					pPitch->_maxNegativeDeviation, pPitch->maxNegDeviationPercentage(),
-					pPitch->_maxPositiveDeviation, pPitch->maxPosDeviationPercentage()
-					);
-			pPitch = pPitch->_next;
-		}
-	}
+	void insertAndMergeANote (Pitch * pPitch);
+	PitchNodeList & getNodeList();
+	void display ();
 
 private:
 	PitchNodeList _pitchNodeList;
